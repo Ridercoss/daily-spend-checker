f67f13dcf143e68e1ce01ecf58ca3e1450d8aad7
L.S.C. Eduardo Cosme Cruz <edcosme91@gmail.com>
2022-08-01 23:40:21 -0500

    Commit Inicial

--- App.jsx
+++ App.jsx
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Touchable } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textHightlight}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2b2b2b',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textHightlight: {
+    "fontSize": 42,
+    "color": "#c1c1c1"
+  }
 });
